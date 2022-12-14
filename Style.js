367825c7cd6041713fa07b6889531c203ee868b5
samira23222 <93806757+samira23222@users.noreply.github.com>
2022-12-14 15:12:07 -0700

    Add files via upload

---
+++ Style.js
@@ -0,0 +1,100 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1
+  },
+
+  imgcontainer: {
+    width: '100%',
+    height: '50%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  
+  bcontainer: {
+    width: '100%',
+    height: '40%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  mcontainer: {
+    width: '100%',
+    height: '10%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  },
+
+  image2: {
+    height: '100%',
+    aspectRatio: 1 / 1
+  },
+
+  sbutton: {
+    height: '90%',
+    aspectRatio: 1 / 1,
+    marginTop: '-30%'
+  },
+  mcontainer: {
+    width: '100%',
+    height: '10%',
+    justifyContent: 'left',
+  
+  },
+  mbutton: {
+    aspectRatio: 1 / 1,
+    height: '90%',
+    marginLeft: "2%",
+    marginBottom: '5%'
+  },
+  hcontainer: {
+    width: '100%',
+    height: '10%',
+    justifyContent: 'left',
+  
+  },
+  hbutton: {
+    aspectRatio: 1 / 1,
+    height: '90%',
+    marginLeft: "1%",
+    marginBottom: '5%'
+  },
+  Pbackground: {
+    width: '100%',
+    height: '50%',
+    justifyContent: 'start',
+    alignItems: 'center',
+    
+  },
+  Pimage: {
+    aspectRatio: 1 / 1,
+    height: '100%',
+  },
+  Magbox: {
+    aspectRatio: 1 / 1,
+    height: '95%',
+    width: '100%',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginTop: '3%',
+    marginLeft: '2.5%',
+    tintColor: '#4E3E7B'
+  }
+
+
+
+});
+
+
+  export { styles };
+  
